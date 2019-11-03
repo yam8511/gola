50e4fde19a87ddc73055d517c48cf41543acea65
Zuolar <yam8511@gmail.com>
2019-11-03 16:39:35 +0800

    star

--- router/default/web.go
+++ router/default/web.go
@@ -2,12 +2,22 @@ package router
 
 import (
 	"gola/app/handler"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
 // LoadWebRouter 載入 router 設定
 func LoadWebRouter(r *gin.Engine) {
+	r.GET("/favicon.png", func(c *gin.Context) {
+		referer, err := url.Parse(c.Request.Referer())
+		if err == nil && referer.Path == "/star/" {
+			c.File("./public/star/favicon.png")
+			return
+		}
+
+		c.Status(404)
+	})
 	// 頁面進入點
 	r.NoRoute(handler.HomeFile())
 	// r.Use(handler.HomeFile())
